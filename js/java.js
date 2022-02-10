a0d72f5ca200573a3b84219794ab0d7dbd2120a3
ehab0barakat <95265260+ehab0barakat@users.noreply.github.com>
2022-02-10 03:35:30 +0200

    Update java.js

--- js/java.js
+++ js/java.js
@@ -7,8 +7,14 @@ $(this).siblings().find("a").removeClass("Active") ;
 
 
 $(".tags .btn").on("click" , function(){
-$(this).removeClass("btn-dark")
-$(this).addClass("btn-warning")
-$(this).siblings().removeClass("btn-warning")
-$(this).siblings().addClass("btn-dark")
-})
+$(this).removeClass("btn-dark").siblings().removeClass("btn-warning")
+$(this).addClass("btn-warning").siblings().addClass("btn-dark")
+if ( $(this).data("feature") == "All" ){
+    $(".shuffle img ").css("opacity" , "1" )
+}
+else{
+    $(".shuffle img ").css("opacity" , ".09" )
+    $(`.shuffle  .${$(this).data("feature")}` ).css("opacity" , "1" )   
+}})
+
+
